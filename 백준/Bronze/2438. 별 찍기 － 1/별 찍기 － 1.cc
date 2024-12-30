6ffee8a7a8d4512749b14dca4432aa9ebd83f31f
Sumin Kim <ksmin5271@khu.ac.kr>
2024-12-30 23:13:38 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		for (int _ = 0; _ < i; _++)
+			cout << "*";
+		cout << "\n";
+	}
+	return 0;
+}
