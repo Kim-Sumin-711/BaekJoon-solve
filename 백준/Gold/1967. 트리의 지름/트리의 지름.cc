951db243fa36b0443d9fe4634fa5a5ecf094f605
Sumin Kim <ksmin5271@khu.ac.kr>
2025-02-06 17:07:52 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 2996 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,68 +1,45 @@
 #include<iostream>
 #include<vector>
-#include<unordered_map>
-#include<queue>
 
-//https://edder773.tistory.com/271 를 참고함.
-//먼저 아무 노드를 선택하고, 그로부터 가장 먼 노드를 찾아낸다. 이름을 N이라고 하자.
-//그리고 N으로부터 가장 거리가 먼 노드를 찾아낸다. M이라고 하자.
-//트리의 지름은 N과 M사이의 거리이다.
+//10305019번 제출 참고해서 DFS풀이를 나타내보았다.
 
+int node_cnt=0;
+//vector의 index는 시작지점, pair.first는 도착하는 지점, second는 그 사이의 거리.
+std::vector<std::vector<std::pair<int, int>>> graph(10001);
+//최대 거리를 저장할 곳과 두번째로 dfs를 사용할 때 시작할 지점을 전역변수로 저장.
+int max_dis = 0, one_point = 0;
 
-using TREE = std::unordered_map<int, std::vector<std::pair<int, int>>>;
-
-void setup_tree(TREE& tree, int node_cnt) {
-	int n1, n2, c;
-	while (--node_cnt) {
-		std::cin >> n1 >> n2 >> c;
-		tree[n1].push_back(std::make_pair(n2, c));
-		tree[n2].push_back(std::make_pair(n1, c));
+//now는 시작 지점. passed 는 바로 전 단계에서 지났던 지점. dis는 현재까지 이동한 거리.
+void dfs(int now, int passed, int dis=0) {
+	//다음으로 갈 곳 있으면 재귀돌고 아니면 이 for문은 스킵되겠다.
+	for (const std::pair<int, int>& route : graph[now]) {
+		//전에서 왔던 곳을 중복해서 가고싶지는 않음.
+		if(route.first != passed)
+			dfs(route.first, now, dis + route.second);
 	}
-}
-//{dis,num}
-std::pair<int,int> get_the_most_far_node_info(const int& target, TREE& tree, const int& node_cnt) {
-	std::vector<int>cost_vec(node_cnt + 1, -1);
-	cost_vec[target] = 0;
-	std::priority_queue<std::pair<int, int>> node_qu;
-	node_qu.push({cost_vec[target],target});
-	std::pair<int, int> now;
-	while (!node_qu.empty()) {
-		now = node_qu.top();
-		node_qu.pop();
-		if (now.first < cost_vec[now.second]) continue;
-		if (tree.find(now.second) != tree.end()) {
-			for (const std::pair<int,int>& line : tree[now.second]) {
-				if (cost_vec[line.first] != -1) continue;
-				if (cost_vec[line.first] < cost_vec[now.second] + line.second) {
-					cost_vec[line.first] = cost_vec[now.second] + line.second;
-					node_qu.push(std::make_pair(cost_vec[line.first], line.first));
-				}
-			}
-		}
+	//만일 최대 거리보다 더 멀리 왔으면 갱신하고 현재 노드를 one_point에 저장함.
+	//첫번째로 1부터 가장 먼 노드 찾을때 그 노드를 one_point에 저장하고
+	//다음으로 이 함수를 돌릴때 now에 넣기 위함.
+	if (max_dis < dis) {
+		max_dis = dis;
+		one_point = now;
 	}
-	int max = -1, max_idx = -1;
-	for (int node = 1; node < node_cnt + 1; node++) {
-		if (cost_vec[node] > max) {
-			max = cost_vec[node];
-			max_idx = node;
-		}
-	}
-	return std::make_pair(max, max_idx);
-}
-
-int get_diameter_of_tree(TREE& tree, const int& node_cnt) {
-	int start_node = 1;
-	int far_node = get_the_most_far_node_info(start_node, tree,node_cnt).second;
-	int res = get_the_most_far_node_info(far_node, tree,node_cnt).first;
-	return res;
 }
 
 int main(void) {
 	std::ios::sync_with_stdio(0); std::cin.tie(0);
-	int node_cnt;
 	std::cin >> node_cnt;
-	TREE tree_map;
-	setup_tree(tree_map,node_cnt);
-	std::cout << get_diameter_of_tree(tree_map, node_cnt);
+	int start, goal, cost;
+	//setup graph
+	while (--node_cnt) {
+		std::cin >> start >> goal >> cost;
+		graph[start].push_back(std::make_pair(goal, cost));
+		graph[goal].push_back(std::make_pair(start, cost));
+	}
+	//1으로부터 가장 먼 노드의 번호를 one_point에 저장한다.
+	dfs(1, 1);
+	//one_point로부터 가장 먼 노드의 번호를 저장함과 동시에 지름을 max_dis에 저장.
+	dfs(one_point, one_point);
+	std::cout << max_dis;
 	return 0;
 }
