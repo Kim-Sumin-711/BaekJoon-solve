1ca2c48ba430531d14b649d131098845cad24b54
Sumin Kim <ksmin5271@khu.ac.kr>
2025-03-15 16:04:32 +0900

    [Gold IV] Title: N-Queen, Time: 4180 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<vector>
 #include<cmath>
+
+//N개의 퀸을 N*N보드판에 배치해야 하므로
+//모든 행에 각각 하나씩 배치해야만 정답임.
+//한 행에 하나씩
+
 int answer = 0;
 
 class Queen {
@@ -19,10 +24,11 @@ public:
 	}
 };
 
-bool is_attacked(const std::vector<Queen>& queens, int row, int col,int queen_cnt) {
-	for (int i = 0; i < queen_cnt;i++) {
+bool is_attacked(const std::vector<Queen>& queens, int row, int col) {
+	for (int i = 0; i < row ;i++) {
 		const Queen& queen = queens[i];
-		if (queen.get_row() == row || queen.get_col() == col)
+		//한 행에 하나씩만 배치하도록 설계했으니 getrow와 row비교할 필요는 없음.
+		if (queen.get_col() == col)
 			return true;
 		else if (std::abs(queen.get_row() - row) == std::abs(queen.get_col() - col))
 			return true;
@@ -30,22 +36,20 @@ bool is_attacked(const std::vector<Queen>& queens, int row, int col,int queen_cn
 	return false;
 }
 
-void sol_Nqueen(std::vector<Queen>& queens, int r, int N, int now_queen_cnt=0) {
+void sol_Nqueen(std::vector<Queen>& queens, int r, int N) {
 	if (r == N) {
+		answer++;
 		return;
 	}
 	else {
 		for (int col = 0; col < N; col++) {
-			if (!is_attacked(queens, r, col, now_queen_cnt)) {
-				if (now_queen_cnt + 1 == N) {
-					answer++;
-					return;
-				}
-				else {
-					queens[now_queen_cnt] = Queen(r, col);
-					sol_Nqueen(queens, r + 1, N, now_queen_cnt + 1);
-				}
+			if (!is_attacked(queens, r, col)) {
+				queens[r] = Queen(r, col);
+				sol_Nqueen(queens, r + 1, N);
+				//공격 받고 있지 않은 경우 위치 정보를 queens에 저장하고
+				//row를 1증가시킴. N이 되면 전부 하나씩 배치 성공했다는 의미.
 			}
+			//공격받고 있는 경우 그 칸은 고려할 필요가 없음. 백트래킹.
 		}
 		
 	}
