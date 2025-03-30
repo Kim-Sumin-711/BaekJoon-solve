b3e5e807c378aa7257d92608f628eb305e9dc9a6
Sumin Kim <ksmin5271@khu.ac.kr>
2025-03-30 17:20:45 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -3,101 +3,82 @@
 using Matrix = std::vector<std::vector<int>>;
 
 void set_matrix(Matrix& matrix) {
-	for (std::vector<int>& each_row : matrix) {
-		for (int& elem : each_row) {
+	for (std::vector<int>& row : matrix) {
+		for (int& elem : row) {
 			std::cin >> elem;
 		}
 	}
 }
 
-void set_two_find(std::vector<long long>& two_find) {
-	int loop = 38;
-	long long twos = 1;
-	while (loop--) {
-		two_find.push_back(twos);
-		twos *= 2;
-	}
-}
-
-void make_matrix_elems_small(Matrix& m) {
-	for (std::vector<int>& row : m) {
-		for (int& elem : row) {
-			if (elem >= 1000) elem %= 1000;
-		}
-	}
-}
-
-Matrix matrix_multiply(const Matrix& m1, const Matrix& m2) {
-	Matrix result(m1.size(), std::vector<int>(m1.size(), 0));
-	for (int row = 0; row < m1.size(); row++) {
-		for (int col = 0; col < m1.size(); col++) {
+Matrix multiply_matrix(const Matrix& m1, const Matrix& m2) {
+	int size = m1.size();
+	Matrix result(size, std::vector<int>(size,0));
+	for (int row = 0; row < size; row++) {
+		for (int col = 0; col < size; col++) {
 			int sum = 0;
-			for (int i = 0; i < m1.size(); i++) {
-				sum += (m1[row][i] * m2[i][col]);
-			}
+			for (int i = 0; i < size; i++)
+				sum += m1[row][i] * m2[i][col];
 			result[row][col] = sum;
 		}
 	}
-	make_matrix_elems_small(result);
 	return result;
 }
 
-short set_two_check_and_get_true_cnt(std::vector<bool>& two_check, const std::vector<long long>& two_find, long long cal_cnt) {
-	short cnt = 0;
-	while (cal_cnt > 1) {
-		for (int i = 1; i < 38; i++) {
-			if (cal_cnt < two_find[i]) {
-				cal_cnt -= two_find[i-1];
-				two_check[i-1] = true;
-				cnt++;
-				break;
-			}
-		}
-	}
-	if (cal_cnt == 1) {
-		cnt++;
-		two_check[0] = true;
-	}
-	return cnt;
-}
-
 void print_matrix(const Matrix& matrix) {
-	for (const std::vector<int>& each_row : matrix) {
-		for (const int& elem : each_row) {
+	for (const std::vector<int>& row : matrix) {
+		for (const int& elem : row)
 			std::cout << elem << " ";
-		}
 		std::cout << "\n";
 	}
 }
 
-void sol(const long long& cal_cnt, Matrix& original_matrix) {
-	std::vector<long long> two_find;
-	set_two_find(two_find);
-	std::vector<bool> two_check(37,false);
-	short cnt = set_two_check_and_get_true_cnt(two_check, two_find, cal_cnt);
-	Matrix result_matrix(original_matrix.size(),std::vector<int>(original_matrix.size(),0));
-	for (int i = 0; i < original_matrix.size(); i++) {
-		result_matrix[i][i] = 1;
-	}
-	for (int i = 0; i < 37; i++) {
-		if (cnt == 0) break;
-		if (two_check[i]) {
-			cnt--;
-			result_matrix = matrix_multiply(result_matrix, original_matrix);
+void make_each_elem_under_1000(Matrix& matrix) {
+	for (std::vector<int>& row : matrix) {
+		for (int& elem : row) {
+			if (elem >= 1000)
+				elem %= 1000;
 		}
-		original_matrix = matrix_multiply(original_matrix, original_matrix);
 	}
-	print_matrix(result_matrix);
+}
+
+Matrix get_result(long long calc_cnt, const Matrix& matrix) {
+	if (calc_cnt == 1)
+		return matrix;
+	else {
+		Matrix divided_matrix = get_result(calc_cnt / 2, matrix);
+		if (calc_cnt % 2) {
+			Matrix result = (multiply_matrix(divided_matrix, divided_matrix));
+			make_each_elem_under_1000(result);
+			result = multiply_matrix(result, matrix);
+			make_each_elem_under_1000(result);
+			return result;
+		}
+		else {
+			Matrix result = multiply_matrix(divided_matrix, divided_matrix);
+			make_each_elem_under_1000(result);
+			return result;
+		}
+	}
+}
+
+void sol(const long long& calc_cnt, Matrix& matrix) {
+	if (calc_cnt == 1) {
+		make_each_elem_under_1000(matrix);
+		print_matrix(matrix);
+	}
+	else {
+		Matrix result = get_result(calc_cnt, matrix);
+		print_matrix(result);
+	}
 }
 
 int main(void) {
 	std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
-	//initalization
 	int size;
-	long long cal_cnt;
-	std::cin >> size >> cal_cnt;
+	long long calc_cnt;
+	std::cin >> size >> calc_cnt;
 	Matrix matrix(size, std::vector<int>(size, 0));
 	set_matrix(matrix);
-	sol(cal_cnt, matrix);
+	sol(calc_cnt, matrix);
 	return 0;
 }
