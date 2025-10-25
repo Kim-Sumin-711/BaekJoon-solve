9a064be62d0a7da2a6d176be814ad2f393194324
Sumin Kim <ksmin5271@khu.ac.kr>
2025-10-25 17:33:10 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,11 @@
+text = input()
+if(text == 'M'):
+    print("MatKor")
+elif(text == 'W'):
+    print("WiCys")
+elif(text == 'C'):
+    print("CyKor")
+elif(text == 'A'):
+    print("AlKor")        
+elif(text == '$'):
+    print("$clear")    
