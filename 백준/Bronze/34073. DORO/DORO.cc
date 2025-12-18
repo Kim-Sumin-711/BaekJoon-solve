229e94bf8aa91cc3aab46217ed398f132e59fab8
Sumin Kim <ksmin5271@khu.ac.kr>
2025-12-18 19:49:29 +0900

    [Bronze IV] Title: DORO, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+int main(void) {
+	int n; std::cin >> n;
+	while (n--) {
+		std::string text;
+		std::cin >> text;
+		std::cout << text << "DORO ";
+	}
+	return 0;
+}
