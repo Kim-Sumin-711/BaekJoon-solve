deaa22a1bca5873d22dba4cd0e83537eec54f5e5
Sumin Kim <ksmin5271@khu.ac.kr>
2026-02-26 19:52:01 +0900

    [Gold III] Title: 줄 세우기, Time: 1244 ms, Memory: 3940 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -3,28 +3,26 @@
 int main(void) {
 	std::ios::sync_with_stdio(0); std::cin.tie(0); std::cout.tie(0);
 	int stu_cnt, compare_cnt; std::cin >> stu_cnt >> compare_cnt;
-	std::vector<std::vector<int>> requires_vec(stu_cnt + 1);
+	std::vector<std::vector<int>> post_vec(stu_cnt + 1);
+	std::vector<int> prior_cnt(stu_cnt + 1,0);
 	std::vector<bool> in_formation(stu_cnt + 1, false);
 	while (compare_cnt--) {
 		int front, back; std::cin >> front >> back;
-		requires_vec[back].push_back(front);
+		post_vec[front].push_back(back);
+		prior_cnt[back]++;
 	}
 	int students_cnt_in_formation = 0;
 	while (students_cnt_in_formation != stu_cnt) {
 		for (int i = 1; i < stu_cnt + 1; i++) {
-			if (in_formation[i] == false) {
-				bool get_in_the_formation = true;
-				for (int required : requires_vec[i]) {
-					if (in_formation[required] == false) {
-						get_in_the_formation = false;
-						break;
-					}
+			if (in_formation[i] == false && prior_cnt[i] == 0) {
+				students_cnt_in_formation++;
+				in_formation[i] = true;
+				for (int post : post_vec[i]) {
+					if(prior_cnt[post] > 0)
+						prior_cnt[post]--;
 				}
-				if (get_in_the_formation == true) {
-					students_cnt_in_formation++;
-					in_formation[i] = true;
-					std::cout << i << ' ';
-				}
+				std::cout << i << ' ';
+				
 			}
 		}
 	}
